804dfdeba253c419c1f172e7739572928744e543
GauravB159 <gbolosta@gmail.com>
2017-03-16 22:59:36 +0530

    Site check

--- JS/stock.js
+++ JS/stock.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
         $(this).addClass("acti");
         var interval= $(this).html();
         $(".data").html("");
+        $(".ticker").html(ticker);
         $.getJSON("http://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval="+interval+"min&apikey=1977", wrapper(interval));
     });
     
@@ -40,49 +41,51 @@ $(document).ready(function(){
     }
     
     var setUpOrDown=function(one,two,arg){
-        var prev=parseFloat(one[arg]);
-        var curr=parseFloat(two[arg]);
-        return prev-curr;
+        var curr=parseFloat(one[arg]);
+        var prev=parseFloat(two[arg]);
+        return (curr-prev)/prev;
     }
     
     var wrapper =function(interval){
          return function(data) {
-            var date2=new Date();
-            var a = moment.tz(date2, "America/Toronto");
-            var date=form(a);
-            date=moment(date).subtract(moment(date).seconds(),'seconds');
-            date=correct(date,9,16,interval);
-            date=form(date);
+            var date=data["Meta Data"]["3. Last Refreshed"];
             var stock=data["Time Series ("+ interval.toString() +"min)"][date.toString()];
-            while(stock == undefined){
-                date=moment(date).set('minutes',moment(date).minutes()-1);
-                date=form(date);
-                stock=data["Time Series ("+ interval.toString() +"min)"][date.toString()];
-            }
-            var comp=moment(date).set({'hours': 16 ,'minutes':0,'seconds': 0});
+            var comp=moment().set({'hours': 16 ,'minutes':0,'seconds': 0});
             comp=moment(comp).subtract(1,'days');            
             comp=form(comp);
+            $('.data').removeClass('green');
+            $('.data').removeClass('red');            
             var stock2=data["Time Series ("+ interval.toString() +"min)"][comp.toString()];
             for(var property in stock2){
                 var curr=setUpOrDown(stock,stock2,property);
                 var cls=property.slice(3,property.length);
+                var val=(100.0*curr).toFixed(2);
                 if(cls=="close"){
                     cls="cl";
                 }
-                if(curr > 0){
+                $('.'+cls).html("("+val+"%)");
+                console.log(val);
+                if(val > 0.0){
                     $('.'+cls).addClass('green');
+                    console.log(cls);
+                    if(cls=="cl"){
+                        $('.ticker').html($('.ticker').html()+" <span class='green'><span class='glyphicon glyphicon-chevron-up'></span>"+"("+val+"%)"+"</span>");       
+                    }
                 }else{
                     $('.'+cls).addClass('red');
+                    if(cls=="cl"){
+                        $('.ticker').html($('.ticker').html()+" <span class='red'><span class='glyphicon glyphicon-chevron-down'></span>"+"("+val+"%)"+"</span>");    
+                    }                       
                 }
             }
             date=moment(date).add(570, 'minutes');
             date=form(date);
             $('.date').html(date.toString() + " IST");
-            $('.open').html(stock["1. open"]);
-            $('.high').html(stock["2. high"]);
-            $('.low').html(stock["3. low"]);
-            $('.cl').html(stock["4. close"]);
-            $('.volume').html(stock["5. volume"]);
+            $('.open').html(stock["1. open"]+$('.open').html());
+            $('.high').html(stock["2. high"]+$('.high').html());
+            $('.low').html(stock["3. low"]+$('.low').html());
+            $('.cl').html(stock["4. close"]+$('.cl').html());
+            $('.volume').html(stock["5. volume"]+$('.volume').html());
         }
     }
     $.getJSON("http://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval=60min&apikey=1977", wrapper(interval));
