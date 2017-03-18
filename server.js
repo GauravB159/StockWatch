a94909fb30ce3e05aa24974be4ef26b6ff48efa6
GauravB159 <gbolosta@gmail.com>
2017-03-18 21:25:55 +0530

    JSON trial

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var request = require("request")
 var fs = require('fs');
+var express=require('express');
+var app=express();
+res.redirect("index.html");
 var url = "http://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=60min&apikey=1977"
 request({
     url: url,
