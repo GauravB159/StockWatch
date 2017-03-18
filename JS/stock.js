cad9fe9c94e55388b7600a55d3f3c463a968ae80
GauravB159 <gbolosta@gmail.com>
2017-03-18 21:04:31 +0530

    JSON trial

--- JS/stock.js
+++ JS/stock.js
@@ -87,23 +87,6 @@ $(document).ready(function(){
             $('.volume').html(stock["5. volume"]+$('.volume').html());
         }
     }
-    var request = require("request")
-    var fs = require('fs');
-    var url = "http://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ticker+"&interval=60min&apikey=1977"
-
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log(body);
-            fs.writeFile("test.json", JSON.stringify(body), function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-            });
-        }
-    });
     $.getJSON("../JS/test.json", wrapper(interval));
     $('[data-toggle="tooltip"]').tooltip(); 
 });
