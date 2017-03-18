4db129c05b086009da63cb21fd72203943a0ae69
GauravB159 <gbolosta@gmail.com>
2017-03-18 21:35:12 +0530

    JSON trial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var request = require("request")
 var fs = require('fs');
 var express=require('express');
+var path    = require("path");
 var app=express();
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
