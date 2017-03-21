aa83f3c4218b80c33e6bd482561a0c4445a291b8
GauravB159 <gbolosta@gmail.com>
2017-03-21 22:23:40 +0530

    Stock update

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 80;
 
-//app.use(express.bodyParser());
-
-//app.use(express.logger());
-
 app.use(express.static(__dirname ));
 
 app.get('/', function(req, res){
-    res.sendfile('index.html', { root: __dirname } );
+    res.sendfile('currency.html', { root: __dirname } );
 });
 
 app.listen(port, function() {
