d945a0ddeb008dd09d2f7a676affecbb4d4acaf3
GauravB159 <gbolosta@gmail.com>
2017-03-21 22:44:36 +0530

    Stock update

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var port = process.env.PORT || 80;
 
 app.use(express.static(__dirname ));
 
-
+app.get("/stock.html",function(req,res){
+    alert("Stock page");
+});
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
