4385e14b54a19701836c1a7ee67b738514ba99b9
GauravB159 <gbolosta@gmail.com>
2017-03-21 22:27:33 +0530

    Stock update

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var port = process.env.PORT || 80;
 
 app.use(express.static(__dirname ));
 
-app.get('/', function(req, res){
-    res.sendfile('index.html', { root: __dirname } );
-});
 
 app.listen(port, function() {
   console.log("Listening on " + port);
