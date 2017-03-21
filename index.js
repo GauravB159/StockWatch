6831bee7f7b1e10075c19b518798f76f78229ba6
GauravB159 <gbolosta@gmail.com>
2017-03-21 23:01:22 +0530

    Stock update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
+var bodyParser=require("body-parser");
 var app = express();
 var port = process.env.PORT || 80;
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(express.static(__dirname ));
 
 app.post('/stock', function(req, res){
+    res.send('You sent the name "' + req.body.ticker + '".');
     res.sendfile('stock.html', { root: __dirname} );
 });
 app.listen(port, function() {
